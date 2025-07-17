7894e3441c1df47534b49fdd913819b54fe13aed
RodrigoDig <rodrigogomesgoncalves5367@gmail.com>
2025-07-17 13:40:48 -0300

    Organização de boas práticas separando por funções

--- src/App.java
+++ src/App.java
@@ -5,26 +5,39 @@ public class App {
 
     static Scanner input = new Scanner(System.in);
 
-    public static void main (String[] args){
-        System.out.printf("=-=-=-JOGO DA VELHA-=-=-=\n");
-
+    public static String[] opcoes(){
         String[] opcoes = {"pedra", "papel", "tesoura"};
+        return opcoes;
+    }
 
+    public static String jogadaJogador(){
         System.out.printf("Informe sua jogada: ");
         String jogada = input.nextLine();
 
-        // Looping de repetição caso o jogador forneça uma jogada inválida
+        return jogada;
+    }
+
+    // Looping de repetição caso o jogador forneça uma jogada inválida
+    public static String validacaoJogada(String jogada){
+
         while (!jogada.equals("pedra") && !jogada.equals("papel") && !jogada.equals("tesoura")) {
             System.out.printf("Jogada inválida! Informe sua jogada: ");
             jogada = input.nextLine();
         }
-        
+
+        return jogada;
+    }
+
+    public static String jogadaMaquina(String[] opcoes){
+
         Random random = new Random();
         int jogadaMaquina = random.nextInt(opcoes.length); // Gera um número entre 0 e 2 (tamanho do array - 1)
         String maquina = opcoes[jogadaMaquina]; // Escolhe uma opção aleatória do array
 
-        System.out.printf("Escolha jogador: %s\n", jogada);
-        System.out.printf("Escolha máquina: %s\n", maquina);
+        return maquina;
+    }
+
+    public static void vencedor(String jogada, String maquina){
 
         if(jogada.equals(maquina)){
             System.out.println("Empate");
@@ -35,5 +48,20 @@ public class App {
         else{
             System.out.println("Máquina venceu!");
         }
+    }
+
+    public static void main (String[] args){
+        System.out.printf("=-=-=-JOGO DA VELHA-=-=-=\n");
+        
+        String[] opcoes = opcoes();
+        String jogada = jogadaJogador();
+        String novaJogada = validacaoJogada(jogada);
+        String maquina = jogadaMaquina(opcoes);
+
+        System.out.printf("Escolha jogador: %s\n", novaJogada);
+        System.out.printf("Escolha máquina: %s\n", maquina);
+
+        vencedor(novaJogada, maquina);
+
     }
 }
